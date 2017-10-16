c28c33f86cb6f0b8db1710537e89d264594d714c
jjproby <jjproby719@gmail.com>
2017-10-16 13:16:32 -0500

    Added two more practice files. They are nothing big. Just stupid stuff I was messing with

---
+++ stupid.js
@@ -0,0 +1,18 @@
+function iFuckingWork() {
+  console.log("I work, you fucking idiot");
+}
+
+function fuckYou() {
+  var img = document.getElementById("fuck");
+  img.src="https://vignette.wikia.nocookie.net/whentheycry/images/5/50/Fuck_you.jpg/revision/latest?cb=20150523075437";
+}
+
+function hateYou() {
+  var img = document.getElementById("fuck");
+  img.src = "http://3.bp.blogspot.com/-AmCTKxQ2lXo/U-N7Yha-QsI/AAAAAAAASUE/kV5OD5W4hIc/s1600/i-hate-you.png"
+}
+
+function reset() {
+  var img = document.getElementById("fuck")
+  img.src = "https://halfelf.org/wp-content/uploads/sites/2/2013/07/yousuck.gif"
+}
